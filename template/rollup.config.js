e82b5035c1986ef54b268b5c83f7b9116cf9401f
Lukas Hirt <lukashirt@mail.com>
2020-11-05 14:05:43 +0100

    Add UI and dev server

---
+++ template/rollup.config.js
@@ -0,0 +1,78 @@
+import vue from 'rollup-plugin-vue'
+import { terser } from 'rollup-plugin-terser'
+import replace from '@rollup/plugin-replace'
+import filesize from 'rollup-plugin-filesize'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+import json from '@rollup/plugin-json'
+import builtins from '@erquhart/rollup-plugin-node-builtins'
+import globals from 'rollup-plugin-node-globals'
+import serve from 'rollup-plugin-serve'
+import copy from 'rollup-plugin-copy'
+import postcss from 'rollup-plugin-postcss'
+
+const production = !process.env.ROLLUP_WATCH
+
+// We can't really do much about circular dependencies in node_modules
+function onwarn (warning) {
+  if (warning.code !== 'CIRCULAR_DEPENDENCY') {
+    console.error(`(!) ${warning.message}`)
+  }
+}
+
+const pipeline = [
+  vue(),
+  replace({
+    'process.env.NODE_ENV': JSON.stringify('production')
+  }),
+  resolve({
+    mainFields: ['browser', 'jsnext', 'module', 'main'],
+    include: 'node_modules/**',
+    preferBuiltins: true
+  }),
+  babel({
+    exclude: 'node_modules/**',
+    runtimeHelpers: true
+  }),
+  commonjs({
+    include: 'node_modules/**'
+  }),
+  json(),
+  globals(),
+  builtins(),
+  production && terser(),
+  production && filesize()
+]
+
+export default [
+  {
+    input: 'ui/src/app.js',
+    output: {
+      file: 'assets/{{ ExtensionId }}.js',
+      format: 'amd'
+    },
+    onwarn,
+    plugins: [
+      ...pipeline,
+      !production && serve('assets')
+    ]
+  },
+  {
+    input: 'ui/dev/src/main.js',
+    output: {
+      file: 'assets/app.js',
+      format: 'iife'
+    },
+    onwarn,
+    plugins: [
+      ...pipeline,
+      !production && copy({
+        targets: [
+          { src: 'ui/dev/public/*', dest: 'assets' }
+        ]
+      }),
+      postcss()
+    ]
+  }
+]
